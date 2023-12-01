6c93ed1e6535454157c29e65c6323a32186adc7f
Andreina Rugama <arc@whitesharkmedia.com>
2023-12-01 12:14:09 -0600

    add configuration for the http request

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,9 +1,21 @@
-import { ApplicationConfig } from '@angular/core';
-import { provideRouter } from '@angular/router';
+import {ApplicationConfig, importProvidersFrom} from '@angular/core';
+import {provideRouter} from '@angular/router';
 
-import { routes } from './app.routes';
-import { provideClientHydration } from '@angular/platform-browser';
+import {routes} from './app.routes';
+import {provideClientHydration} from '@angular/platform-browser';
+import {AuthInterceptorService} from "./core/https/auth-interceptor.service";
+import {provideHttpClient, withFetch, withInterceptors} from "@angular/common/http";
+import {BrowserAnimationsModule} from "@angular/platform-browser/animations";
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideRouter(routes), provideClientHydration()]
+  providers: [
+    importProvidersFrom([
+      BrowserAnimationsModule
+    ]),
+    provideHttpClient(
+      withFetch(),
+      withInterceptors([AuthInterceptorService])
+    ),
+    provideRouter(routes), provideClientHydration()
+  ]
 };
