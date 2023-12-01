eb25b1b6c23f017d6ca877a031c073453b69acea
Andreina Rugama <arc@whitesharkmedia.com>
2023-12-01 10:55:45 -0600

    add missing imports and create service for the responsive of the page

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,15 +1,28 @@
-import { Component } from '@angular/core';
-import { CommonModule } from '@angular/common';
-import { RouterOutlet } from '@angular/router';
-import {SharedModule} from "./shared/shared.module";
+import {Component, HostListener, inject} from '@angular/core';
+import {CommonModule} from '@angular/common';
+import {RouterOutlet} from '@angular/router';
+import {NavBarComponent} from "./shared/components/nav-bar/nav-bar.component";
+import {HttpClientModule} from '@angular/common/http';
+import {ResponsiveService} from "./core/https/responsive.service";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet, SharedModule],
+  imports: [CommonModule, RouterOutlet, NavBarComponent, HttpClientModule],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  styleUrl: './app.component.scss',
 })
 export class AppComponent {
   title = 'banco';
+
+  private responsiveService: ResponsiveService = inject(ResponsiveService);
+
+  @HostListener('window:resize', ['$event'])
+  onResize(event: Window): void {
+    this.responsiveService.setWidthType(event.innerWidth);
+  }
+
+  ngOnInit(): void {
+    this.responsiveService.setWidthType(window.innerWidth);
+  }
 }
