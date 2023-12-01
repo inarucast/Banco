75e82cb531b8d428cc61b73ae82548be8548194f
Andreina Rugama <arc@whitesharkmedia.com>
2023-12-01 12:14:18 -0600

    Update banco.service.ts

--- src/app/core/https/banco.service.ts
+++ src/app/core/https/banco.service.ts
@@ -1,5 +1,5 @@
 import {inject, Injectable} from '@angular/core';
-import {HttpClient} from "@angular/common/http";
+import {HttpClient} from '@angular/common/http';
 import {Observable} from "rxjs";
 import {FinancialProducts} from "../../shared/models/productos-financieros";
 import {environment} from "../../../environments/environment";
