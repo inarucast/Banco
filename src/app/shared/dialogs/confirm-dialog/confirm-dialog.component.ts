db49549c0a75b2152b9afdc42ebe38b67e2b6b1e
Andreina Rugama <arc@whitesharkmedia.com>
2023-12-01 17:13:31 -0600

    create confirm dialog

---
+++ src/app/shared/dialogs/confirm-dialog/confirm-dialog.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import {BehaviorSubject} from "rxjs";
+
+@Component({
+  selector: 'app-confirm-dialog',
+  standalone: true,
+  imports: [],
+  templateUrl: './confirm-dialog.component.html',
+  styleUrl: './confirm-dialog.component.scss'
+})
+export class ConfirmDialogComponent {
+
+}
