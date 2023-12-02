1c4db8a3c76a4bace996b85f70881818e697097a
Andreina Rugama <arc@whitesharkmedia.com>
2023-12-02 09:34:54 -0600

    Update app.config.ts

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -2,9 +2,8 @@ import {ApplicationConfig, importProvidersFrom} from '@angular/core';
 import {provideRouter} from '@angular/router';
 
 import {routes} from './app.routes';
-import {provideClientHydration} from '@angular/platform-browser';
 import {AuthInterceptorService} from "./core/https/auth-interceptor.service";
-import {provideHttpClient, withFetch, withInterceptors} from "@angular/common/http";
+import {provideHttpClient, withInterceptors} from "@angular/common/http";
 import {BrowserAnimationsModule} from "@angular/platform-browser/animations";
 
 export const appConfig: ApplicationConfig = {
@@ -13,9 +12,8 @@ export const appConfig: ApplicationConfig = {
       BrowserAnimationsModule
     ]),
     provideHttpClient(
-      withFetch(),
       withInterceptors([AuthInterceptorService])
     ),
-    provideRouter(routes), provideClientHydration()
+    provideRouter(routes)
   ]
 };
