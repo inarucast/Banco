bd0d75b513349b06a2086120e8853e75d5267075
Andreina Rugama <arc@whitesharkmedia.com>
2023-12-02 09:34:48 -0600

    Update app.component.ts

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,11 @@
 import {Component} from '@angular/core';
 import {RouterOutlet} from '@angular/router';
 import {NavBarComponent} from "./shared/components/nav-bar/nav-bar.component";
-import {HttpClientModule} from '@angular/common/http';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, NavBarComponent, HttpClientModule],
+  imports: [RouterOutlet, NavBarComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
 })
