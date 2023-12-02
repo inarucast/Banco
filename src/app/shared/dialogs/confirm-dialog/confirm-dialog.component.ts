43f816431b9550c7447a1b95e02852ab3a69ad71
Andreina Rugama <arc@whitesharkmedia.com>
2023-12-02 01:47:53 -0600

    create dialog configuration

--- src/app/shared/dialogs/confirm-dialog/confirm-dialog.component.ts
+++ src/app/shared/dialogs/confirm-dialog/confirm-dialog.component.ts
@@ -1,16 +1,40 @@
-import { Component } from '@angular/core';
-import {BehaviorSubject} from "rxjs";
-import {NgIf} from "@angular/common";
+import {Component, Input, Output, EventEmitter, inject} from '@angular/core';
+import {CommonModule} from "@angular/common";
+import {FinancialProducts} from "../../models/productos-financieros";
+import {BancoService} from "../../../core/https/banco.service";
+import {catchError, finalize, throwError} from "rxjs";
+import {SkeletonComponent} from "../../components/skeleton/skeleton.component";
 
 @Component({
   selector: 'app-confirm-dialog',
   standalone: true,
   imports: [
-    NgIf
+    CommonModule,
+    SkeletonComponent
   ],
   templateUrl: './confirm-dialog.component.html',
   styleUrl: './confirm-dialog.component.scss'
 })
 export class ConfirmDialogComponent {
+  @Input() product!: FinancialProducts;
+  @Output() showDialog: EventEmitter<boolean> = new EventEmitter<boolean>();
+
+  bancoService = inject(BancoService);
+  isLoading = false;
+
+  confirmDelete(): void {
+    this.isLoading = true;
+    this.bancoService.deleteProduct(this.product.id).pipe(
+      finalize(() => {
+        this.bancoService.reloadTableSubject.next(true);
+        this.showDialog.emit(false);
+        this.isLoading = false;
+      })
+    ).subscribe();
+  }
+
+  cancelDelete(): void {
+    this.showDialog.emit(false);
+  }
 
 }
